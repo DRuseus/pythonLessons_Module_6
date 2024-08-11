ee1d8f4dcaec36d23f0babb38285a8365cf05704
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-08-11 11:53:38 +0300

    Сделаны правки по заданию "module_6_2": 1. owner = str() __model = str() __engine_power = int() __color = str() Не должно быть атрибутами класса 2. Метод get_model - возвращает строку: "Модель: <название модели транспорта>" Метод get_horsepower - возвращает строку: "Мощность двигателя: <мощность>" Метод get_color - возвращает строку: "Цвет: <цвет транспорта>" Ключевое слово здесь возвращает строку а не функцию print 3. Соответственно из за второго пункта нужно исправить метод print_info 4. Проверка в __COLOR_VARIANTS происходит не учитывая регистр ('BLACK' ~ 'black'). В вашей проверке на set_color сейчас 'BLACK' ~ 'black' два разных элемента сделайте проверку без учета регистра символов

--- Module_6_2/module_6_2.py
+++ Module_6_2/module_6_2.py
@@ -1,32 +1,29 @@
 class Vehicle:
-    owner = str()
-    __model = str()
-    __engine_power = int()
-    __color = str()
     __COLOR_VARIANTS = ('GREEN', 'RED', 'WHITE', 'BLACK', 'BLUE')
+
     def __init__(self, owner, __model, __color, __engine_power):
-        self.owner = owner
-        self.__model = __model
-        self.__engine_power = __engine_power
-        self.__color = __color
+        self.owner = str(owner)
+        self.__model = str(__model)
+        self.__engine_power = int(__engine_power)
+        self.__color = str(__color)
 
     def get_model(self):
-        return print(f'Модель: {self.__model}')
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        return print(f'Мощность двигателя: {self.__engine_power}')
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        return print(f'Цвет: {self.__color}')
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        self.get_model()
-        self.get_horsepower()
-        self.get_color()
-        print(f'Владелец: {self.owner}')
+        print(self.get_model(),
+              self.get_horsepower(),
+              self.get_color(),
+              f'Владелец: {self.owner}', sep='\n')
 
     def set_color(self, new_color):
-        if new_color in self.__COLOR_VARIANTS:
+        if new_color.upper() in self.__COLOR_VARIANTS:
             self.__color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
